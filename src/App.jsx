c247922bd6f46f3d1233e01aa6d9f7e0470617f9
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-09-27 11:36:30 -0700

    getting started

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// import './App.css';
+// import Home from './pages/Home';
+// import Projects from './pages/Projects';
+// import Experience from './pages/Experience';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* <Navbar />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} /> */}
+        </Routes>
+      </Router>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
