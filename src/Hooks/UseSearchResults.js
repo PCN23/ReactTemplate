2006edc4eb89b9ee3020fbb6e90174b97b5f745c
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-08 19:02:28 -0700

    wotking on use search results

--- src/Hooks/UseSearchResults.js
+++ src/Hooks/UseSearchResults.js
@@ -1,5 +1,17 @@
+import { SendAndArchive } from '@mui/icons-material';
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function UseSearchResults() {
+    const [searchParams, setSearchParams] = useSearchParams();
+  const usableSearchParams = Object.fromEntries(
+    searchParams.entries()
+  );
+
+  const nextPage = async () => {
+      usableSearchParams.page = parseInt(usableSearchParams.page) + 1;
+      setSearchParams(usableSearchParams);
+    const moreResults = await SendAndArchive(usableSearchParams);
+  };
   return <div>UseSearchResults</div>;
 }
