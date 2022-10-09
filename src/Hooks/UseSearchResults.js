fee2c55b868b6b0b08a7192f45f85c95d8e362e2
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-08 19:43:18 -0700

    adding use search results

--- src/Hooks/UseSearchResults.js
+++ src/Hooks/UseSearchResults.js
@@ -1,17 +1,51 @@
 import { SendAndArchive } from '@mui/icons-material';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { useSearchParams } from 'react-router-dom';
 
 export default function UseSearchResults() {
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [error, setError] = useState('');
   const usableSearchParams = Object.fromEntries(
     searchParams.entries()
   );
 
   const nextPage = async () => {
-      usableSearchParams.page = parseInt(usableSearchParams.page) + 1;
-      setSearchParams(usableSearchParams);
+    usableSearchParams.page = parseInt(usableSearchParams.page) + 1;
+    setSearchParams(usableSearchParams);
     const moreResults = await SendAndArchive(usableSearchParams);
+    setSearchResults(searchResults.concat(moreResults.results));
   };
-  return <div>UseSearchResults</div>;
+
+  const infiniteScrollRef = useInView({
+    triggerOnce: true,
+    onChange: (inView) => {
+      if (inView) nextPage();
+    },
+  }).ref;
+
+  const searchPokedex = async (searchObj) => {
+    if (searchObj.page === null) {
+      searchObj.page = 1;
+    }
+    setSearchParams(searchObj);
+    try {
+      const body = await search(searchObj);
+      setSearchResults(body.results);
+    } catch (e) {
+      setError('Error searching pokedex ' + e.body.toString());
+    }
+  };
+
+  useEffect(() => void searchPokedex(usableSearchParams), []);
+
+  return {
+    nextPage,
+    searchParams,
+    searchResults,
+    setSearchResults,
+    searchPokedex,
+    infiniteScrollRef,
+  };
 }
