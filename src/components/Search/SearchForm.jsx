56831a7f16e6f8f80a4f352b0c1b1f538725a1b6
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-09 19:08:43 -0700

    installed react intersection observer

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import UseSearchResults from '../../Hooks/UseSearchResults.js';
 import { SelectOption } from '../Forms/FormControls.jsx';
 
-export default function SearchForm({onsubmit}) {
-  const { types } = useSearch();
+export default function SearchForm({ onSubmit }) {
+  const { pokemon, setPokemon } = UseSearchResults();
   const submitHandler = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formDataObject = Object.fromEntries(formData);
-    onSubmit(formData);
+    onSubmit(formDataObject);
   };
 
-return <Form onSubmit={submitHandler}>
-  <SelectOption
-    label="text"
-    name="pokemon"
-    value={pokemon}>
-        onChange={e => setPokemon(e.target.value)}
-    />
-    <FormButton type="submit">Search</FormButton>
+  return (
+    <Form onSubmit={submitHandler}>
+      <SelectOption
+        label="text"
+        name="pokemon"
+        value={pokemon}
+        onChange={(e) => setPokemon(e.target.value)}
+      />
+      <FormButton type="submit">Search</FormButton>
     </Form>
+  );
 }
